a94f1f93b8c277fe626b34874518dc009094d0ad
Saro Deravanesian <saro.deravanesian@gmail.com>
2021-11-09 14:14:48 +0400

    Learning Objects.

---
+++ 09_nov_2021/example_1.js
@@ -0,0 +1,18 @@
+
+
+function isAgeGreaterEqualThan18(age) {
+    return age >= 18;
+}
+
+let result;
+const gender = "male";
+
+// if (gender === "male") {
+//     result = "Mr."
+// } else {
+//     result = "Mrs."
+// }
+
+result = gender === "male" ? "Mr." : "Mrs.";
+
+console.log(result);
---
+++ 09_nov_2021/example_2.js
@@ -0,0 +1,37 @@
+
+const obj = {
+    name: "Gevorg",
+    age: 20
+}
+
+obj["year"] = 1;
+
+// delete obj["year"]; This will remove the key from the object.
+
+console.log(obj.name);
+console.log(obj["name"]);
+
+console.log(obj.age);
+console.log(obj["age"]);
+
+console.log(obj);
+
+function returnValueInObject(obj, key) {
+    // return obj.key Is not valid.
+    return obj[key];
+}
+
+console.log(returnValueInObject(obj, "year"));
+
+console.log(Object.keys(obj));
+console.log(Object.values(obj));
+
+printAllKeyValuePairs(obj);
+
+function printAllKeyValuePairs(obj) {
+    console.log("Using 'in': ");
+    for (let key in obj) {
+        console.log("key is: " + key);
+        console.log("value is: " + obj[key]);
+    }
+}
